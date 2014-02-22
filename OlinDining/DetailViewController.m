6b33d9f2e372f3943650f2feb95c37a74fe9d5b0
Thomas Nattestad <thomasnat1@thomass-imac.olin.edu>
2014-02-22 11:39:32 -0800

    added nutrition labels and ls and loaded into them

--- OlinDining/DetailViewController.m
+++ OlinDining/DetailViewController.m
@@ -33,11 +33,69 @@
 - (void)configureView{
     // Update the user interface for the detail item.
     if (self.food) {
-        self.contentScrollView.contentSize = CGSizeMake(320, 800);
-        
+        self.contentScrollView.contentSize = CGSizeMake(320, 850);
+        /*
+         @property (weak, nonatomic) IBOutlet UILabel *foodNameLabel;
+         @property (weak, nonatomic) IBOutlet UILabel *foodDescriptionLabel;
+         @property (weak, nonatomic) IBOutlet UILabel *foodServSize;
+         @property (weak, nonatomic) IBOutlet UILabel *foodCalories;
+         @property (weak, nonatomic) IBOutlet UILabel *foodFatCalories;
+         @property (weak, nonatomic) IBOutlet UILabel *foodFat;
+         @property (weak, nonatomic) IBOutlet UILabel *foodPercentFatDv;
+         @property (weak, nonatomic) IBOutlet UILabel *foodSatfat;
+         @property (weak, nonatomic) IBOutlet UILabel *foodPercentSatfat;
+         @property (weak, nonatomic) IBOutlet UILabel *foodTransFat;
+         @property (weak, nonatomic) IBOutlet UILabel *foodCholesterol;
+         @property (weak, nonatomic) IBOutlet UILabel *foodPercentCholesterol;
+         @property (weak, nonatomic) IBOutlet UILabel *foodSodium;
+         @property (weak, nonatomic) IBOutlet UILabel *foodPercentSodium;
+         @property (weak, nonatomic) IBOutlet UILabel *foodCarbo;
+         @property (weak, nonatomic) IBOutlet UILabel *foodPercentCarbo;
+         @property (weak, nonatomic) IBOutlet UILabel *foodDfib;
+         @property (weak, nonatomic) IBOutlet UILabel *foodPercentDfib;
+         @property (weak, nonatomic) IBOutlet UILabel *foodSugars;
+         @property (weak, nonatomic) IBOutlet UILabel *foodProtein;
+         @property (weak, nonatomic) IBOutlet UILabel *foodA;
+         @property (weak, nonatomic) IBOutlet UILabel *foodCp;
+         @property (weak, nonatomic) IBOutlet UILabel *foodUp;
+         @property (weak, nonatomic) IBOutlet UILabel *foodIp;
+         @property (weak, nonatomic) IBOutlet UILabel *foodAllergen;
+         @property (weak, nonatomic) IBOutlet UILabel *foodPercentVitADv;
+         @property (weak, nonatomic) IBOutlet UILabel *foodPercentVitCDv;
+         @property (weak, nonatomic) IBOutlet UILabel *foodPercentCalciumDv;
+         @property (weak, nonatomic) IBOutlet UILabel *foodPercentIronDv;
+         */
         NSDictionary *nutrition = [self.food nutrition];
-        self.foodDescriptionLabel.text = [nutrition objectForKey:@"description"];;
         self.foodNameLabel.text = [self.food name];
+
+        self.foodDescriptionLabel.text = [nutrition objectForKey:@"description"];
+        self.foodServSize.text = [nutrition objectForKey:@"serv_size"];
+        self.foodCalories.text = [nutrition objectForKey:@"calories"];
+        self.foodFatCalories.text = [nutrition objectForKey:@"fat_calories"];
+        self.foodFat.text = [nutrition objectForKey:@"fat"];
+        self.foodPercentFatDv.text = [[nutrition objectForKey:@"percent_fat_dv"] stringByAppendingString:@"%"];
+        self.foodSatfat.text = [nutrition objectForKey:@"satfat"];
+        self.foodPercentSatfat.text = [[nutrition objectForKey:@"percent_satfat"] stringByAppendingString:@"%"];
+        self.foodTransFat.text = [nutrition objectForKey:@"trans_fat"];
+        self.foodCholesterol.text = [nutrition objectForKey:@"cholesterol"];
+        self.foodPercentCholesterol.text = [[nutrition objectForKey:@"percent_cholesterol"] stringByAppendingString:@"%"];
+        self.foodSodium.text = [nutrition objectForKey:@"sodium"];
+        self.foodPercentSodium.text = [[nutrition objectForKey:@"percent_sodium"] stringByAppendingString:@"%"];
+        self.foodCarbo.text = [nutrition objectForKey:@"carbo"];
+        self.foodPercentCarbo.text = [[nutrition objectForKey:@"percent_carbo"] stringByAppendingString:@"%"];
+        self.foodDfib.text = [nutrition objectForKey:@"dfib"];
+        self.foodPercentDfib.text = [[nutrition objectForKey:@"percent_dfib"] stringByAppendingString:@"%"];
+        self.foodSugars.text = [nutrition objectForKey:@"sugars"];
+        self.foodProtein.text = [nutrition objectForKey:@"protein"];
+        self.foodA.text = [nutrition objectForKey:@"a"];
+        self.foodCp.text = [nutrition objectForKey:@"cp"];
+        self.foodUp.text = [nutrition objectForKey:@"up"];
+        self.foodIp.text = [nutrition objectForKey:@"ip"];
+        self.foodAllergen.text = [nutrition objectForKey:@"allergen"];
+        self.foodPercentVitADv.text = @"";
+        self.foodPercentVitCDv.text = @"";
+        self.foodPercentCalciumDv.text = @"";
+        self.foodPercentIronDv.text = @"";
     }
 }
 
